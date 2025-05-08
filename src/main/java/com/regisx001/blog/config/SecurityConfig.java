b562f8449a7880fca8ff79db2ccac74b71699d1d
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-05-08 14:15:54 +0100

    refactor: Add Customizer for CORS configuration in SecurityFilterChain

--- src/main/java/com/regisx001/blog/config/SecurityConfig.java
+++ src/main/java/com/regisx001/blog/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,6 +29,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/api/v1/auth/**").permitAll()
