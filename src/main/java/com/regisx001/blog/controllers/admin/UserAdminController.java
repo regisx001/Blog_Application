73ca898d8a58513a0aea2d59c52bf5decd72bcea
regisx001 <zarqi.ezzoubair@etu.uae.ac.ma>
2025-07-24 01:34:39 +0100

    feat: Restore getAllUsers method in UserAdminController; re-enable user retrieval functionality for admin

--- src/main/java/com/regisx001/blog/controllers/admin/UserAdminController.java
+++ src/main/java/com/regisx001/blog/controllers/admin/UserAdminController.java
@@ -1,12 +1,16 @@
 package com.regisx001.blog.controllers.admin;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.regisx001.blog.domain.dto.UserDto;
 import com.regisx001.blog.domain.dto.requests.UpdateUserRoleRequest;
 import com.regisx001.blog.domain.dto.responses.SuccessResponse;
 import com.regisx001.blog.services.UserService;
@@ -20,12 +24,11 @@ public class UserAdminController {
 
     private final UserService userService;
 
-    // @PreAuthorize("hasRole('ADMIN')")
-    // @GetMapping
-    // public ResponseEntity<Page<UserDto>> getAllUsers(Pageable pageable) {
-    // Page<UserDto> users = userService.getAllUsers(pageable);
-    // return ResponseEntity.ok(users);
-    // }
+    @GetMapping
+    public ResponseEntity<Page<UserDto.Detailed>> getAllUsers(Pageable pageable) {
+        Page<UserDto.Detailed> users = userService.getAllUsers(pageable);
+        return ResponseEntity.ok(users);
+    }
 
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping(path = "/set-admin")
